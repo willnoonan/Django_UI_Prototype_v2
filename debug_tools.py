34389618868383be4d2a3bb7a5b55bb4c434e6a7
willnoonan <90508630+willnoonan@users.noreply.github.com>
2022-01-30 10:25:34 -0600

    Update debug_tools.py

--- debug_tools.py
+++ debug_tools.py
@@ -1,6 +1,9 @@
 
 
 def print_request_dir(request, ignore_meta=True):
+    """
+    I use this to look into the attributes of request objects when debugging view functions.
+    """
     request_dir = dir(request)
     filt_req_dir = [y for y in request_dir if not y.startswith('_')] if ignore_meta else [y for y in request_dir]
     for x in filt_req_dir:
